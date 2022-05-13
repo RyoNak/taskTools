8bfb4ca2c2d342e413b95a5cd463e07a7c95aede
RyoNak <xot.es355gib@gmail.com>
2022-05-13 16:53:14 +0900

    タスク追加部分のCSS調整、優先度部分のドラッグ実装

--- components/LayoutFooter.vue
+++ components/LayoutFooter.vue
@@ -12,7 +12,7 @@
 
 <style scoped>
 	footer{
-		position: absolute;
+		position: fixed;
 		bottom: 0;
 		left: 0;
 		width: 100%;
--- pages/newTask.vue
+++ pages/newTask.vue
@@ -4,44 +4,69 @@
     <div class="task-box">
       <dl>
         <dd>
-           <div class="input_box" :class="{'active': isBorder}">
-              <div class="input_inner">
-                 <input id="style_1" type="text" @focus="isBorder = true" @blur="isBorder = false"　v-model="task.title">
-                 <div class="input_string"><span v-if="!task.title">タイトルを入力</span></div>
-              </div>
-           </div>					
+					<div class="input-effect">
+            <input type="text" class="input_1" v-model="task.title" @blur="hasContent">
+            <label>タイトルを入力</label>
+						<span class="focus-border"></span>
+					</div>
 				</dd>
       </dl>
       <dl>
         <dt>期限/日程</dt>
-        <dd><label class="label_style_1"><input type="date" v-model="task.deadline"></label></dd>
+        <dd>
+					<div class="input-effect">
+            <label class="label_style_1">
+              <input type="date" v-model="task.deadline">
+							<div class="icon_input calender"></div>
+            </label>
+					</div>
+				</dd>
       </dl>
 			<dl>
 				<dt>開始時刻</dt>
-				<dd><input type="time" v-model="task.st_time"></dd>
+				<dd>
+					<div class="input-effect">
+						<label class="label_style_1">
+							<input type="time" v-model="task.st_time">
+							<div class="icon_input clock"></div>
+						</label>
+					</div>	
+				</dd>
 			</dl>
 			<dl>
 				<dt>終了時刻</dt>
-				<dd><input type="time" v-model="task.fin_time"></dd>
+				<dd>
+					<div class="input-effect">
+						<label class="label_style_1">
+							<input type="time" v-model="task.fin_time">
+							<div class="icon_input clock"></div>
+						</label>
+					</div>						
+				</dd>
 			</dl>
       <dl>
-        <dt>優先度</dt>
-        <dd><input type="number" v-model="task.priority"></dd>
+        <dt>優先度（1から99まで）</dt>
+				<dd>
+					<div class="drag-parent" id="drag-1" @click="onClick">
+						<div class="drag-child" id="drag-2" :class="{'active': is_drag}" @dragstart="onDrugStart" @drag="onDrug" @dragend="onDrugEnd" draggable></div>
+					</div>
+					<span>{{ task.priority }}</span>
+				</dd>
       </dl>
       <dl>
         <dt>メモ</dt>
-        <dd><textarea v-model="task.memo"></textarea></dd>
+        <dd><textarea class="textarea_1" v-model="task.memo"></textarea></dd>
       </dl>
       <dl>
         <dt>進捗状況</dt>
         <dd>
-          <label><input type="radio" v-model="task.done" value="yet">未着手</label>
-          <label><input type="radio" v-model="task.done" value="doing">処理中</label>
-          <label><input type="radio" v-model="task.done" value="hold">保留</label>
-          <label><input type="radio" v-model="task.done" value="comp">完了</label>
+          <label class="btn btn_1" :class="{'active': task.done === 'yet'}"><input type="radio" v-model="task.done" value="yet">未着手</label>
+          <label class="btn btn_1" :class="{'active': task.done === 'doing'}"><input type="radio" v-model="task.done" value="doing">処理中</label>
+          <label class="btn btn_1" :class="{'active': task.done === 'hold'}"><input type="radio" v-model="task.done" value="hold">保留</label>
+          <label class="btn btn_1" :class="{'active': task.done === 'comp'}"><input type="radio" v-model="task.done" value="comp">完了</label>
         </dd>
       </dl>
-      <button type="button" @click="addTask">追加</button>
+      <button type="button" class="btn btn_2" @click="addTask">追加</button>
     </div>
 	</layout-wrapper>
 </template>
@@ -60,12 +85,15 @@
           memo: '',
           done: ''
         },
-				isBorder: false,
+				is_drag: false,
       }
     },
 		created(){
 			this.setDate();
 			this.setTime();
+		},
+		mounted(){
+			this.init();
 		},
     methods:{
       async addTask(){
@@ -82,12 +110,49 @@
 				const nowTime = date.getHours() + ':' + date.getMinutes();
 				this.task.st_time = nowTime;
 			},
+			init(){
+				const childWidth = document.getElementById('drag-2').offsetWidth;
+				this.task.priority = this.diffPriority(childWidth);
+			},
       formatDate(date) {
         let year = date.getFullYear();
         let month = ("0" + (date.getMonth() + 1)).slice(-2);
         let day = ("0" + date.getDate()).slice(-2);
         return `${year}-${month}-${day}`;
       },	
+			hasContent(e){
+				if(e.target.value !== ''){
+					e.target.classList.add('has-content');
+				}else{
+					e.target.classList.remove('has-content');
+				}
+			},
+			onDrugStart(e){
+				this.is_drag = true;
+				e.dataTransfer.setDragImage(new Image(),0,0);
+			},
+			onDrug(e){
+				e.target.style.width = e.offsetX + 'px';
+			},
+			onDrugEnd(e){
+				this.is_drag = false;
+				const childWidth = e.target.offsetWidth;
+				this.task.priority = this.diffPriority(childWidth);
+			},
+			onClick(e){
+				const childWidth = e.offsetX;
+				this.task.priority = this.diffPriority(childWidth);	
+				document.getElementById('drag-2').style.width = e.offsetX+'px';
+			},
+			// 「優先度」を計算する
+			diffPriority(childVal){
+				const parentWidth = document.getElementById('drag-1').offsetWidth;
+				let diff = Math.round(childVal / parentWidth * 100);
+				if(diff > 100){
+					diff = 100;
+				}
+				return diff;
+			},
     }
   }
 </script>
@@ -95,5 +160,9 @@
 <style scoped>
 	h2{
 		margin-bottom: 2rem;
+	}
+	.task-box{
+		width: 100%;
+		max-width: 500px;
 	}
 </style>
