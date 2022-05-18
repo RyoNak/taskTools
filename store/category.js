6d3d787043af2aefcdeeaeb9c37051a34910c40f
RyoNak <xot.es355gib@gmail.com>
2022-05-18 15:55:55 +0900

    コメントアウト部分を削除、カテゴリを取得、削除する記述を追加

--- store/category.js
+++ store/category.js
@@ -5,20 +5,20 @@ export const actions = {
   addCategory(vuexContext, category){
     vuexContext.commit('add',category);
   },
-/*	async setTask(vuexContext){
-		let task = [];
+	async setCategory(vuexContext){
+		let cats = [];
 		//タスクを空にする
 		vuexContext.commit('remove');
 		//同期的にfirebaseからデータを取得
 		await new Promise((resolve,reject)=>{
-			this.$fire.database.ref('task').on("value",(obj)=>{
+			this.$fire.database.ref('category').on("value",(obj)=>{
           if(obj){
             const rootList = obj.val();
             if(rootList != null) {
                 const keys = Object.keys(rootList);
                 for(var i in keys){
                   rootList[keys[i]].id = keys[i];
-                  task.push(rootList[keys[i]]);
+                  cats.push(rootList[keys[i]]);
                 }
             }
 						resolve(1);
@@ -27,14 +27,11 @@ export const actions = {
 					}
         });
 		});
-		vuexContext.commit('select',task);
+		vuexContext.commit('select',cats);
 	},
-	updateTask(vuexContext,task){
-		vuexContext.commit('update',task);
-	},
-	deleteTask(vuexContext,id){
+	deleteCategory(vuexContext,id){
 		vuexContext.commit('delete',id);
-	},*/
+	},
 }
 export const mutations = {
   add(state,category){
@@ -43,43 +40,24 @@ export const mutations = {
       'icon': category.icon //アイコンの種類
     });		
   },
-/*	select(state,task){
-		state.list.splice(0);
-		for(var i in task){
+	select(state,cats){
+
+		for(var i in cats){
 			const obj = {
-			'id': task[i].id,
-      'title': task[i].title, //タイトル
-      'reg_date': task[i].reg_date, //登録日
-      'deadline': task[i].deadline, //期限
-			'st_time': task[i].st_time, //開始時刻
-			'fin_time': task[i].fin_time,//終了時刻
-      'priority': task[i].priority, //優先度
-      'memo': task[i].memo,//メモ
-      'done': task[i].done //ステータス
+				'id': cats[i].id,
+				'name': cats[i].name,
+      	'icon': cats[i].icon, //タイトル
 			}
 			
-			state.list.push(obj);
+			state.categories.push(obj);
 		}
 	},
 	remove(state){
-		state.list.splice(0);
-	},
-	update(state,task){
-		console.log(task);
-		this.$fire.database.ref('task').child(task.id).update({
-      title: task.title,
-      deadline: task.deadline,
-      st_time: task.st_time,
-      fin_time: task.fin_time,
-      priority: task.priority,
-      memo: task.memo,
-      done: task.done			
-		});
+		state.categories.splice(0);
 	},
 	delete(state,id){
-		console.log(id);
-		this.$fire.database.ref('task').child(id).remove();
-	}*/
+		this.$fire.database.ref('category').child(id).remove();
+	}
 }
 export const getters = {
   getCategories(state){
