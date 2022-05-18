be8e8fbb796612fde796dc43c13d7b2637b6ee74
RyoNak <xot.es355gib@gmail.com>
2022-05-18 15:47:53 +0900

    カテゴリ追加画面にアイコンを追加できるように変更、追加時に画面のリロードをしていた部分をstateを再取得する方法に変更

--- components/Category.vue
+++ components/Category.vue
@@ -4,8 +4,13 @@
       <input type="text" class="input_1" :class="{'has-content': category.name}" v-model="category.name">
       <label>カテゴリ名を入力</label>
       <span class="focus-border"></span>
+    </div>    
+		<div class="input-effect">
+      <input type="text" class="input_1" :class="{'has-content': category.icon}" v-model="category.icon">
+      <label>アイコン（Font Awesomeのiタグを貼り付け）</label>
+      <span class="focus-border"></span>
     </div>
-		<button type="button" class="btn btn_3" @click="addCategory">追加</button>
+		<button type="button" class="btn btn_3" @click="addCategory">追加</button>		
 	</div>
 </template>
 
@@ -23,8 +28,9 @@
 		methods: {
 			async addCategory(){
 				if(this.category.name){
-          await this.$store.dispatch('category/addCategory',this.category);
-          this.$router.push('/newCategory');					
+          await this.$store.dispatch('category/addCategory',this.category);			
+					this.$emit('reload');
+					this.category = {};
 				}
 			},
 		},
@@ -36,5 +42,8 @@
 		width: 100%;
 		max-width: 580px;
 		padding: 2rem 0;
+	}
+	.input-effect + .input-effect{
+		margin-top: 60px;
 	}
 </style>
