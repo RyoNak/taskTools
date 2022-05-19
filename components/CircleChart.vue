423cd45e6378a8751f4e9fa56fb8b907a829166a
RyoNak <xot.es355gib@gmail.com>
2022-05-19 18:01:45 +0900

    円グラフ表示用コンポーネント作成

---
+++ components/CircleChart.vue
@@ -0,0 +1,16 @@
+<script>
+import { Doughnut, mixins } from 'vue-chartjs'
+
+export default {
+  extends: Doughnut,
+  mixins: [mixins.reactiveProp],
+  data () {
+    return {
+      options: {}
+    }
+  },
+  mounted () {
+    this.renderChart(this.chartData, this.options)
+  }
+}
+</script>
