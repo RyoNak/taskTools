c1b71595ee43826036450e127ed1ef6c1f6ba602
RyoNak <xot.es355gib@gmail.com>
2022-05-19 18:01:23 +0900

    優先度グラフを表示する記述を追加、カテゴリ分布グラフを表示する処理記述途中

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,15 @@
 <template>
 	<layout-wrapper>
-		<BarChart :chartdata="chartdata"/>
+		<div class="graff-container flex-base between">
+      <div class="graff-box">
+        <h2>優先度</h2>
+        <BarChart :styles="BarStyles" :chartData="chartPriority"/>
+      </div>
+      <div class="graff-box">
+        <h2>カテゴリ分布</h2>
+				<CircleChart :chartData="chartCategory"/>
+      </div>
+		</div>
 	</layout-wrapper>
 </template>
 
@@ -9,22 +18,20 @@
 export default{
   data(){
     return {
-      chartdata: {
-        labels: ['January','Feburary','March','April'],
-        datasets: [
-          {
-            label: ['Data One'],
-            backgroundColor: '#f87979',
-            data: [40,30,10,25]
-          }
-        ]
-      },				
+      chartPriority: {},	
+      chartCategory: {},	
     }
   },
   computed: {
     taskList(){
-      return this.$store.getters['todo/getList'];
+      //return this.$store.getters['todo/getList'];
+			return this.$store.state.todo.list;
     },		
+		BarStyles(){
+			return {
+				width: '100%',
+			}
+		},
   },
 	watch:{
 		taskList:{
@@ -32,16 +39,43 @@ export default{
 				const c_data = JSON.parse(JSON.stringify(this.taskList));
 				const titles =  c_data.map(v=>v.title);
 				const priorities =  c_data.map(v=>v.priority);
+				const categories = c_data.map(v=>v.category);
+				let counts = {};
 				
-				console.log(priorities);
-				this.chartdata.labels = titles;
-				this.chartdata.datasets[0].data = priorities;
+				for(var i in categories){
+					const key = categories[i];
+					counts[key] = (counts[key])? counts[key]+1: 1;
+				}
+				
+				/*優先度グラフ用*/
+				this.chartPriority = {
+          labels: titles,
+          datasets: [
+            {
+              label: ['優先度'],
+              backgroundColor: '#f87979',
+              data: priorities
+            }
+          ]					
+				};
+				/*カテゴリ分布グラフ用*/
+				
 			},
 			deep: true
 		},
 	},
 	created(){
 		this.setTask();
+				this.chartData = {
+          labels: ['loading'],
+          datasets: [
+            {
+              label: ['Data One'],
+              backgroundColor: '#f87979',
+              data: [1]
+            }
+          ]					
+				};		
 	},
 	methods:{
 		async setTask(){
