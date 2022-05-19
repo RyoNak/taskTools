5551e429e514f87ef2cd3882405f571a5e18beda
RyoNak <xot.es355gib@gmail.com>
2022-05-19 18:00:08 +0900

    リアクティブに再描写するためにmixinsにreactivePropを追加

--- components/BarChart.vue
+++ components/BarChart.vue
@@ -1,23 +1,16 @@
 <script>
-	import { Bar } from 'vue-chartjs';
-	export default{
-		extends: Bar,
-		props:{
-			chartdata:{
-				type: Object,
-				default: ()=>{},
-				required: true,
-			},
-			options:{
-				type: Object,
-				default: ()=>({
-					responsive: true,
-					maintainAspectRatio: false
-				}),
-			},
-		},
-		mounted(){
-			this.renderChart(this.chartdata,this.options);
-		},
-	}
+import { Bar, mixins } from 'vue-chartjs'
+
+export default {
+  extends: Bar,
+  mixins: [mixins.reactiveProp],
+  data () {
+    return {
+      options: {}
+    }
+  },
+  mounted () {
+    this.renderChart(this.chartData, this.options)
+  }
+}
 </script>
