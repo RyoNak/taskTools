8b49d4ceff1c997ef9bf522eb1ad565cca42c008
RyoNak <xot.es355gib@gmail.com>
2022-05-23 11:53:22 +0900

    h2見出し用コンポーネントの作成

---
+++ components/Midashi.vue
@@ -0,0 +1,17 @@
+<template>
+	<h2 class="midashi"><i class="fa-solid fa-crown mr"></i><slot/></h2>
+</template>
+
+<script>
+ export default{
+	 name: 'Midashi',
+ }
+</script>
+
+<style scoped>
+	h2{
+		padding: 1rem;
+		margin-bottom: 2rem;
+		position: relative;
+	}
+</style>
