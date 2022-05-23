a2050a708a54cb34ec73a82b79e78f89949a1321
RyoNak <xot.es355gib@gmail.com>
2022-05-23 11:57:49 +0900

    グラフ説明テーブルの追加、それに伴い優先度、カテゴリのまとめ方を一部変更

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,37 @@
 	<layout-wrapper>
 		<div class="graff-container flex-base between">
 			<div class="graff-box">
-				<h2>優先度 TOP 5</h2>
+				<Midashi>優先度 TOP 5</Midashi>
+				<!--<h2></h2>-->
 				<BarChart :styles="BarStyles" :chartData="chartPriority" />
+				<div class="table-graff">
+          <dl class="flex-base al-center between table-head">
+            <dt class="wid10">順位</dt>
+            <dt class="wid65">タスク名</dt>
+            <dt class="wid25 right">優先度スコア</dt>
+          </dl>
+          <dl v-for="(t,index) in rank_priority" class="flex-base al-center between" >
+            <dd class="wid10"><i class="fa-solid" :class="'fa-'+ (index + 1)"></i></dd>
+            <dd class="wid65">{{ t.title }}</dd>
+            <dd class="wid25 right">{{ t.priority }}</dd>
+          </dl> 
+				</div>
 			</div>
 			<div class="graff-box">
-				<h2>カテゴリ分布</h2>
+				<Midashi>カテゴリ分布</Midashi>
 				<CircleChart :chartData="chartCategory" />
+				<div class="table-graff">
+          <dl class="flex-base al-center between table-head">
+            <dt class="wid10">順位</dt>
+            <dt class="wid65">カテゴリ名</dt>
+            <dt class="wid25 right">割合(％)</dt>
+          </dl>
+          <dl v-for="(t,index) in rank_category" class="flex-base al-center between" >
+            <dd class="wid10"><i class="fa-solid" :class="'fa-'+ (index + 1)"></i></dd>
+            <dd class="wid65">{{ t.title }}</dd>
+            <dd class="wid25 right">{{ Math.round((t.quant / all_category_length) * 100) }}%</dd>
+          </dl> 
+				</div>				
 			</div>
 		</div>
 	</layout-wrapper>
@@ -24,51 +49,56 @@ export default {
 	},
 	computed: {
 		taskList() {
-			//return this.$store.getters['todo/getList'];
-			return this.$store.state.todo.list;
+			return JSON.parse(JSON.stringify(this.$store.state.todo.list));
 		},
 		BarStyles() {
 			return {
 				width: '100%',
 			}
 		},
+		rank_priority(){
+			return this.taskList.map(v=>{
+				return {title: v.title, priority: v.priority};
+			}).sort((a,b)=>{ return b.priority - a.priority}).slice(0,5);
+		},
+		rank_category(){
+      const categories = this.taskList.map(v => v.category);
+      let counts = {};
+
+      for (var i in categories) {
+        const key = categories[i];
+        counts[key] = (counts[key]) ? counts[key] + 1 : 1;
+      }
+			return Object.entries(counts).map(([key,value])=>{ return {title: key,quant: value}}).sort((a,b)=>{ return b.quant - a.quant});
+		},
+		all_category_length(){
+			let length =  0;
+			this.rank_category.map(v=>{
+				length += v.quant;
+			});
+			return length;
+		}
 	},
 	watch: {
 		taskList: {
 			handler(newVal, oldVal) {
 				/*優先度グラフ用*/
-				const c_data = JSON.parse(JSON.stringify(this.taskList));
-				const c_data_sorted = c_data.sort((a,b)=>b.priority - a.priority);	
-				const c_data_sliced = c_data_sorted.slice(0,5);
-
-				const titles = c_data_sliced.map(v => v.title);
-				const priorities = c_data_sliced.map(v => v.priority);
-
 				this.chartPriority = {
-					labels: titles,
+					labels: this.rank_priority.map(v => v.title),
 					datasets: [
 						{
 							label: ['優先度'],
-							data: priorities
+							data: this.rank_priority.map(v => v.priority)
 						}
 					]
 				};
 				/*カテゴリ分布グラフ用*/
-
-				const categories = c_data.map(v => v.category);
-				let counts = {};
-
-				for (var i in categories) {
-					const key = categories[i];
-					counts[key] = (counts[key]) ? counts[key] + 1 : 1;
-				}
-
 				this.chartCategory = {
-					labels: Object.keys(counts),
+					labels: this.rank_category.map(v=>v.title),
 					datasets: [
 						{
 							label: ['タスクの比率'],
-							data: Object.values(counts)
+							data: this.rank_category.map(v=>v.quant)
 						}
 					]
 				}
@@ -96,3 +126,18 @@ export default {
 	},
 }
 </script>
+
+<style>
+	.table-graff{
+		margin-top: 2rem;
+		font-size: 0.9rem;
+	}
+	.table-graff dl dt{
+		margin-bottom: 0;
+	}
+	.table-graff dl dd{
+		overflow: hidden;
+		text-overflow: ellipsis;
+		white-space: nowrap;
+	}
+</style>
